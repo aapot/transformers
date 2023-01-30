ca06b3563f373f15f6b34c9827fbe6c6a1bf0720
aapot <aapo@lingoes.ai>
2023-01-30 21:24:09 +0200

    add eval_steps and save_steps handling

--- examples/research_projects/jax-projects/wav2vec2/run_wav2vec2_pretrain_flax.py
+++ examples/research_projects/jax-projects/wav2vec2/run_wav2vec2_pretrain_flax.py
@@ -455,8 +455,9 @@ def main():
     num_epochs = int(training_args.num_train_epochs)
     train_batch_size = int(training_args.per_device_train_batch_size) * jax.device_count()
     eval_batch_size = int(training_args.per_device_eval_batch_size) * jax.device_count()
-
-    num_train_steps = len(vectorized_datasets["train"]) // train_batch_size * num_epochs
+    num_train_samples = len(vectorized_datasets["train"])
+    num_eval_samples = len(vectorized_datasets["validation"])
+    num_train_steps = num_train_samples // train_batch_size * num_epochs
 
     # Create learning rate schedule
     warmup_fn = optax.linear_schedule(
@@ -605,7 +606,6 @@ def main():
         rng, input_rng = jax.random.split(rng)
 
         # Generate an epoch by shuffling sampling indices from the train dataset
-        num_train_samples = len(vectorized_datasets["train"])
         # Avoid using jax.numpy here in case of TPU training
         train_samples_idx = np.random.permutation(np.arange(num_train_samples))
         train_batch_idx = generate_batch_splits(train_samples_idx, train_batch_size, drop_last=True)
@@ -638,37 +638,47 @@ def main():
 
                 train_metrics = []
 
-        # ======================== Evaluating ==============================
-        num_eval_samples = len(vectorized_datasets["validation"])
-        # Avoid using jax.numpy here in case of TPU training
-        eval_samples_idx = np.arange(num_eval_samples)
-        eval_batch_idx = generate_batch_splits(eval_samples_idx, eval_batch_size, drop_last=False)
+            if (cur_step + 1) % training_args.eval_steps == 0:
+                # ======================== Evaluating ==============================
+                # Avoid using jax.numpy here in case of TPU training
+                eval_samples_idx = np.arange(num_eval_samples)
+                eval_batch_idx = generate_batch_splits(eval_samples_idx, eval_batch_size, drop_last=False)
 
-        eval_metrics = []
-        for i, batch_idx in enumerate(tqdm(eval_batch_idx, desc="Evaluating ...", position=2)):
-            samples = [vectorized_datasets["validation"][int(idx)] for idx in batch_idx]
-            model_inputs = data_collator(samples)
+                eval_metrics = []
+                for i, batch_idx in enumerate(tqdm(eval_batch_idx, desc="Evaluating ...", position=2)):
+                    samples = [vectorized_datasets["validation"][int(idx)] for idx in batch_idx]
+                    model_inputs = data_collator(samples)
 
-            # Model forward
-            model_inputs = shard(model_inputs.data)
-            metrics = p_eval_step(state.params, model_inputs)
-            eval_metrics.append(metrics)
+                    # Model forward
+                    model_inputs = shard(model_inputs.data)
+                    metrics = p_eval_step(state.params, model_inputs)
+                    eval_metrics.append(metrics)
 
-        # get eval metrics
-        eval_metrics = get_metrics(eval_metrics)
-        eval_metrics = jax.tree_util.tree_map(jnp.mean, eval_metrics)
+                # get eval metrics
+                eval_metrics = get_metrics(eval_metrics)
+                eval_metrics = jax.tree_util.tree_map(jnp.mean, eval_metrics)
 
-        # Update progress bar
-        log_str = f"Epoch... {epoch + 1}/{num_epochs} "
-        for k, v in eval_metrics.items():
-            log_str += "| {}: {:.3e}".format(k, v.item())
-        epochs.write(log_str)
+                # Update progress bar
+                log_str = f"Epoch... {epoch + 1}/{num_epochs} "
+                for k, v in eval_metrics.items():
+                    log_str += "| {}: {:.3e}".format(k, v.item())
+                epochs.write(log_str)
 
-        # Save metrics
-        if has_tensorboard and jax.process_index() == 0:
-            cur_step = epoch * (len(vectorized_datasets["train"]) // train_batch_size)
-            write_eval_metric(summary_writer, eval_metrics, cur_step)
+                # Save metrics
+                if has_tensorboard and jax.process_index() == 0:
+                    write_eval_metric(summary_writer, eval_metrics, cur_step)
 
+            if (cur_step + 1) % training_args.save_steps == 0:
+                # save checkpoint and push checkpoint to the hub
+                if jax.process_index() == 0:
+                    params = jax.device_get(jax.tree_util.tree_map(lambda x: x[0], state.params))
+                    model.save_pretrained(training_args.output_dir, params=params)
+                    if training_args.push_to_hub:
+                        repo.push_to_hub(
+                            commit_message=f"Saving weights and logs of step {cur_step + 1}", blocking=False
+                        )
+
+        # ======================== End of epoch ==============================
         # save checkpoint after each epoch and push checkpoint to the hub
         if jax.process_index() == 0:
             params = jax.device_get(jax.tree_util.tree_map(lambda x: x[0], state.params))
